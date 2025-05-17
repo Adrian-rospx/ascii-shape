8eadfb3f55340a6f2fc1e9e141feebc515c84d7f
Adrian-rospx <adrianrosu5400@gmail.com>
2025-05-17 23:57:01 +0300

    added: basic line drawing algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,22 @@ std::vector<std::string> screen(
     std::string(WIDTH, ' ')
 );
 
+struct Point {
+    float x;
+    float y;
+};
+
+void draw_line_alg(float x1, float y1, float x2, float y2) {
+    float dx = x2 - x1;
+    float dy = y2 - y1;
+    float m = dy / dx;      // m = slope
+
+    for(int x = std::round(x1); x <= std::round(x2); x++) {
+        const int y = static_cast<int>(m * (x-x1) + y1);
+        screen.at(y).at(x) = 'y';
+    }
+}
+
 // rendering function (flips screen for convenience)
 void render() {
     for(auto it = screen.rbegin(); it != screen.rend(); ++it) {
@@ -23,6 +39,8 @@ void render() {
 int main() {
     screen.at(0).at(0) = '@';
     screen.at(HEIGHT - 1).at(WIDTH - 1) = '@';
+
+    draw_line_alg(0, 16, 15, 5);
 
     render();
 
