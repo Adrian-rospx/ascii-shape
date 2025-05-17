29555a7a1d73645d8f17364910aac15908287c3b
Adrian-rospx <adrianrosu5400@gmail.com>
2025-05-18 00:06:52 +0300

    fix: line drawing working from all directions

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,21 @@ struct Point {
 };
 
 void draw_line_alg(float x1, float y1, float x2, float y2) {
+    // switch points if the x values aren't in order
+    if(x1 > x2) {
+        float temp = x1;
+        x1 = x2;
+        x2 = temp;
+
+        temp = y1;
+        y1 = y2;
+        y2 = temp;
+    }
     float dx = x2 - x1;
     float dy = y2 - y1;
-    float m = dy / dx;      // m = slope
+    float m = dy / dx;  // m = slope
 
+    // drawing:
     for(int x = std::round(x1); x <= std::round(x2); x++) {
         const int y = static_cast<int>(m * (x-x1) + y1);
         screen.at(y).at(x) = 'y';
@@ -40,7 +51,7 @@ int main() {
     screen.at(0).at(0) = '@';
     screen.at(HEIGHT - 1).at(WIDTH - 1) = '@';
 
-    draw_line_alg(0, 16, 15, 5);
+    draw_line_alg(15, 16, 2, 5);
 
     render();
 
